02cf0c7284f86f1db998fcb1ef252ed9d4b07aa4
Frudakisk <frudakisk@gmail.com>
2024-09-06 17:50:31 -0600

    added some checking functions
    check if bookname chapter and translation are real... not done with checking if verse is real

--- scriptureDb.py
+++ scriptureDb.py
@@ -2,15 +2,25 @@ from tinydb import TinyDB, Query
 import requests
 import re
 
-#add scripture into database
-#make sure scripture does not already exist in the database before adding it
-#give a flow of creating scripture untill user is done
+#add scripture into database (DONE)
+#make sure scripture does not already exist in the database before adding it (DONE)
+#give a flow of creating scripture untill user is done (DONE)
 #instead of mapping, try to use a class (DONE)
 
-#I think I should be using a bible api so that the user does not have to write out the entire verse each time
+#I think I should be using a bible api so that the user does not have to write out the entire verse each time (DONE)
 
 #API needs translation, bookId, chapter number, and verse number
 #I should allow people to input multiple verses such as John 3:16-18
+
+'''
+TODO:
+Check if the book is real
+Check if the chapter number is real
+Check if the verse number is real
+Check if the translation is real
+'''
+
+translationsBook = "https://bolls.life/static/bolls/app/views/translations_books.json"
 
 class Scripture:
     def __init__(self, reference: str, bookName: str, chapter: int, verse: int, text: str, translation: str):
@@ -25,7 +35,6 @@ class Scripture:
 #init a database and store is as a JSON file
 db = TinyDB("scriptures.json")
 
-s = Scripture("John 3:16", "John", 3, 16, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life", "NIV")
 
 #Insert scripture into the database
 #make sure it is not already there
@@ -48,6 +57,7 @@ def InsertScripture(s: Scripture) -> bool:
         print("this scripture already exist in our database")
         return False
     
+
 #Cuts reference into its three parts: Book name, chapter number, verse number
 #returns a list of these items in the respective order
 def SpliceScripture(reference: str) -> list:
@@ -66,6 +76,7 @@ def SpliceScripture(reference: str) -> list:
     items = [items[0], temp[0], temp[1]]
     return items
 
+
 #Replaces <br> tags with newlines, and all other html tags with ''
 def RemoveHtmlTags(text:str) -> str:
     """Replaces <br> tags with newlines, and all other html tags with ''
@@ -79,6 +90,7 @@ def RemoveHtmlTags(text:str) -> str:
     clean = re.sub(r'<br.*?>', '\n', text)
     clean = re.sub(r'<.*?>', '', clean)
     return clean
+
 
 def CreateAPIVerse(translation: str, bookId: int, scriptureItems: list) -> dict:
     """_summary_
@@ -97,7 +109,86 @@ def CreateAPIVerse(translation: str, bookId: int, scriptureItems: list) -> dict:
     return apiData
 
 
+def GetTranslationBookData() -> dict:
+    """Gets data from the constant url about translations and the books in that bible translation
+
+    Returns:
+        dict: a json format dictionary that contains bible book information for all kinds of translations
+    """
+    response = requests.get(translationsBook)
+    data = response.json()
+    return data
+
+
+def IsTranslationReal(data: dict, translation: str) -> bool:
+    """Checks if translation is an item in data
+
+    Args:
+        data (dict): a JSON format dictionary that contains bible translations and books in that translation
+        translation (str): translation user wants verse in
+
+    Returns:
+        bool: True if translation is in data, false if not
+    """
+    if translation in data:
+        return True
+    else:
+        return False
+
+
+def IsBookReal(data: dict, scriptureItems: list, translation: str) -> bool:
+    """Checks if the book name is a real book in the current decided translation.
+    The translation variable should already be varified as true before using this method
+
+    Args:
+        data (dict): a JSON format dictonary that contains bible translations and books in that translation
+        scriptureItems (list): A scripture reference split up by book name, chapter number, and verse number
+        translation (str): translation user wants verse in
+
+    Returns:
+        bool: returns True if book is in translation, false otherwise
+    """
+    #check if the book name is even a real one within the translation
+    bookName = scriptureItems[0]
+
+    for item in data[translation]: #we should be checking if this translation is even real FIRST
+        if bookName == item['name']:
+            return True
+        
+    return False
+
+
+def IsChapterReal(data: dict, scriptureItems: list, translation: str) -> bool:
+    """Checks if the chapter number of the scripture reference is a real chapter
+
+    Args:
+        data (dict): a JSON format dictonary that contains bible translations and books in that translation
+        scriptureItems (list): A scripture reference split up by book name, chapter number, and verse number
+        translation (str): translation user wants verse in
+
+    Returns:
+        bool: returns True if chapter is real, false otherwise
+    """
+    bookName = scriptureItems[0]
+    chapterNumber = int(scriptureItems[1])
+
+    for item in data[translation]:
+        if bookName == item['name']:
+            maxChapters = item['chapters']
+            if chapterNumber <= maxChapters:
+                return True
+    
+    return False
+
+
+def IsVerseReal(scriptureItems: list, translation: str) -> bool:
+    url = f"https://bolls.life/get-text/{translation}/{bookId}/{scriptureItems[1]}/"
+
+
+
+
 isOn = True
+data = GetTranslationBookData()
 while(isOn):
     print("enter 'done' to end process")
     reference = input("Reference: ")
@@ -108,6 +199,8 @@ while(isOn):
 
     translation = input("Translation: ")
 
+
+
     #format reference
     reference = reference.capitalize().strip()
     #parse input to grab book name, chapter num and verse num
@@ -115,10 +208,12 @@ while(isOn):
     #format translation
     translation = translation.upper().strip()
 
-    #Find books involved in users input translation - Request JSON from API
-    translationsBook = "https://bolls.life/static/bolls/app/views/translations_books.json"
-    response = requests.get(translationsBook)
-    data = response.json()
+    print("Checking Chapter\n")
+    print(IsChapterReal(data, scriptureItems, translation))
+
+        #check if reference is even real by book, chapter, verse in that order
+    if IsBookReal(scriptureItems, translation) and IsChapterReal(bookName) and IsVerseReal():
+        pass
 
     booksInTranslation = None
     bookId = None
